2b4f8ed4a3501b92e148ed3ae1bcfae6ed552f44
Fina <finaadr@gmail.com>
2019-08-22 19:58:03 +0300

    Js_THP

---
+++ script_test.js
@@ -0,0 +1,11 @@
+console.log("coucou depuis la console");
+let data1 = "variable en let définie dans le bloc principal";
+
+{ //début du sous-bloc
+
+  data1 = "variable en let modifiée dans le sous-bloc";
+  var data2 = "variable en var définie dans le sous-bloc";
+  let data3 = "variable en let définie dans le sous-bloc";
+
+} //fin du sous-bloc
+
